a546a0837218c5a8867d0ed54e0c5f3d4ab8231a
windstorm <likunarmstrong@gmail.com>
2012-10-22 17:36:23 -0600

    Copy different bashrc file based on the OS on host

--- bash/install.sh
+++ bash/install.sh
@@ -1,7 +1,17 @@
 echo "***************Bash*****************"
 echo "Please modify your PS1 to be: "
 echo "export PS1=\"\u@\h:\W #\! \A \`if [ \$? == 0 ]; then echo \:\); else echo \:\(; fi\` \""
-cp `pwd`/bash/bashrc $HOME/.bashrc
+
+linux="Linux"
+mac="Darwin"
+if [ `uname -s` == $linux ]
+then 
+    cp `pwd`/bash/bashrc_linux $HOME/.bashrc
+elif [ `uname -s` == $mac ]
+then
+    cp `pwd`/bash/bashrc_mac $HOME/.bashrc
+fi
+
 if [ $? -eq 0 ]
 then
     echo "$(tput setaf 2)Copy bashrc...OK!$(tput sgr0)"
