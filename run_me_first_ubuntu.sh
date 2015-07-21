0cc13025dd8a43cef72e3d87829d7b58496ee802
Kun <likunarmstrong@gmail.com>
2015-07-20 18:56:02 -0600

    Remove vim-latexsuite

--- run_me_first_ubuntu.sh
+++ run_me_first_ubuntu.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y python-pip python-dev python-setuptools
 sudo easy_install pylint
 
 # Vim
-sudo apt-get install -y vim vim-scripts vim-doc vim-latexsuite vim-gui-common vim-gnome
+sudo apt-get install -y vim vim-scripts vim-doc vim-gui-common vim-gnome
 
 # Git
 sudo apt-get install -y git
